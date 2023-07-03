ce0ed7a144cdab706cab95a047f6cbb82cb48ab8
designershao <designer.shaoyan@gmail.com>
2023-07-03 20:42:00 +0800

    ✨: add pingreq packet to_bytes method.

--- src/protocol/v3/pingreq.rs
+++ src/protocol/v3/pingreq.rs
@@ -1,3 +1,4 @@
+use bytes::BytesMut;
 use nom::{IResult, combinator::map};
 
 use super::fixed_header::{FixHeader, self};
@@ -17,8 +18,16 @@ pub fn parse(input: &[u8]) -> IResult<&[u8], PingreqPacket> {
     })(input)
 }
 
+impl PingreqPacket {
+    pub fn to_bytes(&self) -> BytesMut {
+        self.fix_header.to_bytes()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use nom::AsBytes;
+
     use crate::protocol::v3::fixed_header::PacketType;
 
     use super::*;
@@ -28,5 +37,22 @@ mod tests {
         let input = &[0xC0,0x00];
         let out = parse(input).unwrap();
         assert!(out.1.fix_header.packet_type == PacketType::PINGREQ);
+    }
+
+    #[test]
+    fn test_to_bytes() {
+        let fix_header = FixHeader {
+            packet_type: PacketType::PINGREQ,
+            qos: None,
+            retain: None,
+            dup: None,
+            remaining_length: 0,
+        };
+
+        let pingreq_packet = PingreqPacket {
+            fix_header
+        };
+
+        assert_eq!(pingreq_packet.to_bytes().as_bytes(), &[0xC0, 0x00]);
     }
 }
