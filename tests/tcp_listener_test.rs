b7b629062d36fa72a525698b1febf590139900f0
designershao <designer.shaoyan@gmail.com>
2023-12-06 23:46:53 +0800

    ✅: add invalid mqtt connect packet test.

--- tests/tcp_listener_test.rs
+++ tests/tcp_listener_test.rs
@@ -221,3 +221,98 @@ pub async fn test_tcp_client_subscribe_and_publish() {
     pub_join.await.unwrap();
 
 }
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+pub async fn test_tcp_client_invalid_connect_packet_should_disconnect() {
+
+    let plugin_manager =get_test_plugin_manager().await;
+    let session_manager = Arc::new(RwLock::new(SessionManager{ session_table:  HashMap::<String,RwLock<HashMap<String, SessionHandle>>>::new()}));
+    let topic_manager = Arc::new(RwLock::new(TopicManager::new()));
+
+    let keep_live_duration_secs = 5;
+
+    let resend_duration_secs = 10;
+
+    let (router_sender, router_receiver) = tokio::sync::mpsc::channel(10);
+
+    let mut router = Router {
+        session_manager: session_manager.clone(),
+        topic_manager: topic_manager.clone(),
+        router_receiver: router_receiver,
+    };
+
+    tokio::spawn(async move {
+        router.run().await;
+    });
+
+    let settings = Settings {
+        session: samoye::settings::Session { qos_expired_secs: 2, packet_resend_interval_secs: resend_duration_secs },
+        listener: samoye::settings::Listener { tcp: samoye::settings::Tcp { external: "0.0.0.0:18088".to_string() }},
+        plugin: samoye::settings::Plugin { dir: "test".to_string() }
+    };
+
+    let listener = MqttTcpListener {
+        plugin_manager,
+        session_manager: session_manager.clone(),
+        topic_manager: topic_manager.clone(),
+        router_sender: router_sender,
+        settings: Arc::new(settings),
+    };
+
+    tokio::spawn(async move {
+        listener.run().await.unwrap();
+    });
+
+
+    // subscriber process
+    let invalid_connect_join = tokio::spawn(async move {
+
+        let variable_header = samoye::protocol::v3::connect::VariableHeader {
+                protocol_name: "MQT".to_string(), // invalid protocol name
+                protocol_level: 0x04,
+                username_flag: true,
+                password_flag: true,
+                will_retain: true,
+                will_qos: 1,
+                will_flag: true,
+                clean_session: true,
+                keep_alive: 0,
+            };
+        let payload = samoye::protocol::v3::connect::Payload {
+            client_identifier: "MQTT".to_string(),
+            will_topic: Some("MQTT".to_string()),
+            will_message: Some("MQTT".to_string()),
+            username: Some("MQTT".to_string()),
+            password: Some("MQTT".to_string()),
+        };
+
+        let fix_header = samoye::protocol::v3::fixed_header::FixHeader{
+                packet_type: samoye::protocol::PacketType::CONNECT,
+                qos: None,
+                retain: None,
+                dup: None,
+                remaining_length: variable_header.get_length() + payload.get_length(),
+            };
+
+        let connect_packet = samoye::protocol::v3::connect::ConnectPacket {
+            fix_header,
+            variable_header,
+            payload
+        };
+
+        let mut invalid_connect = tokio::net::TcpStream::connect("0.0.0.0:18088").await.unwrap();
+        let connect_packet = samoye::protocol::MqttPacketV3::Connect(connect_packet);
+        invalid_connect.write(&connect_packet.to_bytes()).await.unwrap();
+        invalid_connect.flush().await.unwrap();
+
+        let mut buf = Vec::new();
+        let read_bytes = invalid_connect.read_buf(&mut buf).await.unwrap();
+        if read_bytes == 0 {
+            assert!(true)
+        } else {
+            assert!(false)
+        }
+    });
+
+    invalid_connect_join.await.unwrap();
+}
