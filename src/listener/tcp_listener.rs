0684bb2d906d5c5196da423a8f2f894458f67930
designershao <designer.shaoyan@gmail.com>
2023-12-19 05:50:16 +0000

    ✅: tcp listener first packet invalid unit test.

--- src/listener/tcp_listener.rs
+++ src/listener/tcp_listener.rs
@@ -43,12 +43,38 @@ impl MqttTcpListener {
             let settings = self.settings.clone();
 
             tokio::spawn(async move {
-                let mut connection = Connection::new(stream);
+                let mut connection: Connection<tokio::net::TcpStream> = Connection::new(stream);
                 // wait the first connect packet, if the packet is not correct, the connection will be closed.
-                let first_packet = connection.read_packet().await.unwrap();
+                let first_packet = connection.read_packet().await;
 
-                match first_packet {
+                if first_packet.is_err() {
+                    warn!("read first packet error: {}", first_packet.unwrap_err());
+                    return
+                } 
+
+                match first_packet.unwrap() {
                     crate::protocol::MqttPacketV3::Connect(packet) => {
+                        // invalid mqtt protocol name
+                        if packet.variable_header.protocol_name != "MQTT" {
+                            warn!("invalid mqtt protocol name");
+                            connection.shutdown().await.unwrap();
+                            return
+                        }
+                        //
+
+                        // unsupport protocol version
+                        if packet.variable_header.protocol_level != 4 {
+                            let connack_packet = ConnAckPacketBuilder::new().set_return_code(crate::protocol::v3::connack::ConnackReturnCode::UnsupportedProtocolVersion).build();
+                            if let Err(e) = connection.write_packet(&crate::protocol::MqttPacketV3::Connack(connack_packet)).await {
+                                warn!("write connack packet error: {}", e);
+                                connection.shutdown().await.unwrap();
+                                return
+                            }
+                            connection.shutdown().await.unwrap();
+                            return
+                        }
+                        //
+
                         let r = plugin_manager
                             .clone()
                             .call_hook_on_connect_auth(&peer_addr.to_string(), &packet)
@@ -202,6 +228,55 @@ mod tests {
             .await
             .unwrap();
         Arc::new(plugin_manager)
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    pub async fn test_invalid_connect_packet_income() {
+
+        let plugin_manager =get_test_plugin_manager().await;
+
+        let keep_live_duration_secs = 5;
+
+        let resend_duration_secs = 10;
+
+        let (router_sender, router_receiver) = tokio::sync::mpsc::channel(10);
+
+        let settings = Settings {
+            session: crate::settings::Session { qos_expired_secs: 2, packet_resend_interval_secs: resend_duration_secs },
+            listener: crate::settings::Listener { tcp: crate::settings::Tcp { external: "127.0.0.1:18088".to_string() }},
+            plugin: crate::settings::Plugin { dir: "test".to_string() }
+        };
+
+        let listener = MqttTcpListener {
+            plugin_manager,
+            session_manager: Arc::new(RwLock::new(SessionManager{ session_table:  HashMap::<String,RwLock<HashMap<String, SessionHandle>>>::new()})),
+            topic_manager: Arc::new(RwLock::new(TopicManager::new())),
+            router_sender: router_sender,
+            settings: Arc::new(settings),
+        };
+
+        tokio::spawn(async move {
+            listener.run().await.unwrap();
+        });
+
+        let mut writer = tokio::net::TcpStream::connect("0.0.0.0:18088").await.unwrap();
+        let connect_packet = crate::protocol::v3::connect::ConnectPacketBuilder::new("test".to_string())
+            .clean_session(true)
+            .keep_alive(keep_live_duration_secs)
+            .build();
+
+
+        let invalid_mqtt_packet = &[0x10, 0x28,0x00,0x08,0x4D,0x51,0x54,0x54,0x04,0xEE,0x00,0x00,0x00,0x04,0x4D,0x51,0x54,0x54,0x00,0x04,0x4D,0x51,0x54,0x54,0x00,0x04,0x4D,0x51,0x54,0x54,0x00,0x04,0x4D,0x51,0x54,0x54,0x00,0x04,0x4D,0x51,0x54,0x54];
+        writer.write(invalid_mqtt_packet).await.unwrap();
+        writer.flush().await.unwrap();
+
+        let mut buf = Vec::new();
+        let read_bytes = writer.read_buf(&mut buf).await.unwrap();
+        if read_bytes == 0 {
+            assert!(true)
+        } else {
+            assert!(false)
+        }
     }
 
 
