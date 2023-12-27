16400cc43ad5200e2aba70d4229893d4057cc159
designershao <designer.shaoyan@gmail.com>
2023-12-27 23:22:39 +0800

    🚧: process wss listener.

---
+++ src/listener/wss_listener.rs
@@ -0,0 +1,73 @@
+use std::{sync::Arc, time::Duration, fs::File};
+
+use anyhow::Result;
+use log::warn;
+use tokio::{net::TcpListener, select, sync::RwLock, io::{AsyncRead, AsyncWrite}};
+use tokio_native_tls::native_tls::{Identity, self};
+
+use crate::{
+    connection::Connection,
+    inflight::Inflight,
+    plugin::{self, plugin_manager::PluginManager, session_context::SessionContext},
+    protocol::{
+        v3::{
+            connack::{ConnAckPacket, ConnAckPacketBuilder, VariableHeader},
+            fixed_header::FixHeader,
+        },
+        PacketType,
+    },
+    router::RouterCmd,
+    session::{Session, SessionHandle, SessionManager, SessionManagerError},
+    settings::Settings,
+    topic::TopicManager,
+};
+
+use super::accept_connection;
+pub struct MqttWssListener {
+    pub plugin_manager: Arc<PluginManager>,
+    pub session_manager: Arc<RwLock<SessionManager>>,
+    pub topic_manager: Arc<RwLock<TopicManager>>,
+    pub router_sender: tokio::sync::mpsc::Sender<RouterCmd>,
+    pub settings: Arc<Settings>,
+}
+
+impl MqttWssListener {
+
+    pub async fn run(self) -> Result<()> {
+
+        let mut cert_file = File::open(&self.settings.listener.wss.cert_file)?;
+        let mut key_file = File::open(&self.settings.listener.wss.key_file)?;
+
+        let mut cert = vec![];
+        cert_file.read_to_end(&mut cert).unwrap();
+
+        let mut key = vec![];
+        key_file.read_to_end(&mut key).unwrap();
+        
+        let cert = Identity::from_pkcs8(&cert, &key)?;
+
+        let tls_acceptor = tokio_native_tls::TlsAcceptor::from(native_tls::TlsAcceptor::builder(cert).build()?);
+
+        let listener = TcpListener::bind(self.settings.listener.wss.external.clone()).await?;
+        loop {
+            let (stream, _) = listener.accept().await?;
+
+            let peer_addr = stream.peer_addr().unwrap();
+            let plugin_manager = self.plugin_manager.clone();
+            let session_manager = self.session_manager.clone();
+            let topic_manager = self.topic_manager.clone();
+            let router_sender = self.router_sender.clone();
+            let settings = self.settings.clone();
+
+            let remote_addr = stream.peer_addr().unwrap();
+
+            let mut tls_stream = tls_acceptor.accept(stream).await.unwrap();
+
+            let ws_stream = tokio_tungstenite::accept_async(tls_stream).await.unwrap();
+
+            tokio::spawn(
+                accept_connection(ws_stream, plugin_manager, session_manager, topic_manager, router_sender, settings, peer_addr)
+            );
+        }
+    }
+}
