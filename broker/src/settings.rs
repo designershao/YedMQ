cd4d45dfb95fdc1a2789320c91e89687827592e3
designershao <designer.shaoyan@gmail.com>
2024-12-12 13:20:13 +0800

    ✨: Add default settings.

--- broker/src/settings.rs
+++ broker/src/settings.rs
@@ -51,22 +51,49 @@ impl<'de> Deserialize<'de> for DefaultAuthorizationValue {
     }
 }
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 pub struct Mqtt {
     pub sys_topic_interval_secs: u64, // system topic interval seconds
     pub default_authentication: DefaultAuthenticationValue,
     pub default_authorization: DefaultAuthorizationValue,
 }
 
-#[derive(Debug,Deserialize,Default)]
+impl Default for Mqtt {
+    fn default() -> Self {
+        Self {
+            sys_topic_interval_secs: 10,
+            default_authentication: DefaultAuthenticationValue::default(),
+            default_authorization: DefaultAuthorizationValue::default()
+        }
+    }
+}
+
+#[derive(Debug,Deserialize)]
 pub struct Session {
     pub qos_expired_secs: u64, // qos context expired seconds
     pub packet_resend_interval_secs: u64 // session packet resend interval seconds
 }
 
-#[derive(Debug, Deserialize,Default)]
+impl Default for Session {
+    fn default() -> Self {
+        Self {
+            qos_expired_secs: 10,
+            packet_resend_interval_secs: 10
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Plugin {
     pub dir: String // plugin dir path
+}
+
+impl Default for Plugin {
+    fn default() -> Self {
+        Self {
+            dir: "./plugins".to_string()
+        }
+    }
 }
 
 #[derive(Debug, Deserialize,Default)]
@@ -78,33 +105,78 @@ pub struct Listener {
     pub api: Api
 }
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 pub struct TcpTls {
     pub external: String,
     pub cert_file: String,
     pub key_file: String
 }
 
-#[derive(Debug, Deserialize, Default)]
+impl Default for TcpTls {
+    fn default() -> Self {
+        Self {
+            external: "0.0.0.0:8883".to_string(),
+            cert_file: "".to_string(),
+            key_file: "".to_string()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Ws {
     pub external: String
 }
 
-#[derive(Debug, Deserialize, Default)]
+impl Default for Ws {
+    fn default() -> Self {
+        Self {
+            external: "0.0.0.0:8083".to_string()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Wss {
     pub external: String,
     pub cert_file: String,
     pub key_file: String
 }
 
-#[derive(Debug, Deserialize, Default)]
+impl Default for Wss {
+    fn default() -> Self {
+        Self {
+            external: "0.0.0.0:8084".to_string(),
+            cert_file: "".to_string(),
+            key_file: "".to_string()
+        }
+    }
+    
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Tcp {
     pub external: String
 }
 
-#[derive(Debug, Deserialize, Default)]
+impl Default for Tcp {
+    fn default() -> Self {
+        Self {
+            external: "0.0.0.0:1883".to_string()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Api {
     pub external: String
+}
+
+impl Default for Api {
+    fn default() -> Self {
+        Self {
+            external: "0.0.0.0:3456".to_string()
+        }
+    }
 }
 
 
@@ -112,6 +184,21 @@ impl Settings {
 
     pub fn new() -> Result<Self, ConfigError> {
         let s = Config::builder()
+            .set_default("mqtt.default_authentication", "allow")?
+            .set_default("mqtt.default_authorization", "allow")?
+            .set_default("mqtt.sys_topic_interval_secs", 10)? 
+            .set_default("session.qos_expired_secs", 10)?
+            .set_default("session.packet_resend_interval_secs", 10)?
+            .set_default("plugin.dir", "./plugins")?
+            .set_default("listener.tcp.external", "0.0.0.0:1883")?
+            .set_default("listener.tcp_tls.external", "0.0.0.0:8883")?
+            .set_default("listener.tcp_tls.cert_file", "")?
+            .set_default("listener.tcp_tls.key_file", "")?
+            .set_default("listener.ws.external", "0.0.0.0:8083")?
+            .set_default("listener.wss.external", "0.0.0.0:8084")?
+            .set_default("listener.wss.cert_file", "")?
+            .set_default("listener.wss.key_file", "")?
+            .set_default("listener.api.external", "0.0.0.0:3456")?
             .add_source(
                 File::with_name("/etc/samoye/config.toml").required(false)
             )
