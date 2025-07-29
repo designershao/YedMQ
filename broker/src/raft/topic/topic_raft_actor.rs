8edc5da50b58f8b3ccec77d3d34591ad60f88a5c
designershao <designer.shaoyan@gmail.com>
2025-07-29 09:44:03 +0800

    🚧: Implement topic raft actor (handle message logic)

--- broker/src/raft/topic/topic_raft_actor.rs
+++ broker/src/raft/topic/topic_raft_actor.rs
@@ -1,11 +1,20 @@
-use std::{cell::OnceCell, clone, path::Path, sync::Arc};
+use std::{cell::OnceCell, path::Path, sync::Arc};
 
 use actix::prelude::*;
-use openraft::{error::{ClientWriteError, RaftError}, Config};
+use openraft::{
+    error::{ClientWriteError, RaftError},
+    Config,
+};
 use tokio::sync::RwLock;
 use yedmq_mqtt::MqttPacketV3;
 
-use crate::{raft::{topic::{raft_network_impl::Network, store::new_storage, types::TopicRaft}, Node, NodeId}, topic::topic_storage::{TopicStorage}};
+use crate::{
+    raft::{
+        topic::{raft_network_impl::Network, store::new_storage, types::TopicRaft},
+        Node, NodeId,
+    },
+    topic::topic_storage::TopicStorage,
+};
 
 #[derive(Debug, Clone)]
 pub enum ActorState {
@@ -17,7 +26,6 @@ pub enum ActorState {
 
 #[derive(Clone, Debug, thiserror::Error)]
 pub enum TopicRaftError {
-
     #[error("Invalid topic name: {topic}")]
     InvalidTopicName { topic: String },
 
@@ -38,7 +46,6 @@ pub enum TopicRaftError {
 
     #[error("Raft initialization error: {0}")]
     RaftInitializationError(String),
-
 }
 
 pub struct TopicRaftActor {
@@ -49,7 +56,9 @@ pub struct TopicRaftActor {
 }
 
 impl TopicRaftActor {
-    async fn initialize_raft(settings: Arc<crate::settings::Settings>) -> Result<TopicRaft,TopicRaftError>  {
+    async fn initialize_raft(
+        settings: Arc<crate::settings::Settings>,
+    ) -> Result<TopicRaft, TopicRaftError> {
         let raft_config = Config {
             cluster_name: "yedmq_topic_cluster".to_string(),
             ..Default::default()
@@ -71,12 +80,27 @@ impl TopicRaftActor {
             network,
             log_store,
             state_machine_store,
-        ).await.map_err(|e| TopicRaftError::RaftInitializationError(e.to_string()))
+        )
+        .await
+        .map_err(|e| TopicRaftError::RaftInitializationError(e.to_string()))
     }
 
     fn process_pending_messages(&mut self, ctx: &mut Context<Self>) {
-        // TODO: Handle pending messages
+        for msg in self.pending_messages.drain(..) {
+            if let Some(subscribe_msg) = msg.downcast_ref::<Subscribe>() {
+                ctx.address().do_send(subscribe_msg.clone());
+            } else if let Some(unsubscribe_msg) = msg.downcast_ref::<Unsubscribe>() {
+                ctx.address().do_send(unsubscribe_msg.clone());
+            } else if let Some(register_msg) = msg.downcast_ref::<RegisterRetainPublishPacket>() {
+                ctx.address().do_send(register_msg.clone());
+            } else if let Some(clean_msg) = msg.downcast_ref::<CleanRetainPublishPacket>() {
+                ctx.address().do_send(clean_msg.clone());
+            } else {
+                log::warn!("Unknown pending message type: {:?}", msg);
+            }
+        }
     }
+
 }
 
 impl Default for TopicRaftActor {
@@ -93,26 +117,27 @@ impl Default for TopicRaftActor {
 
 impl SystemService for TopicRaftActor {
     fn service_started(&mut self, ctx: &mut Context<Self>) {
-        let raft = self.raft.clone();
         let settings = self.settings.clone();
         let addr = ctx.address();
 
-        ctx.spawn(async move {
-            let raft_instance = Self::initialize_raft(settings).await;
-            addr.do_send(InitializationComplete(raft_instance));
-        }.into_actor(self));
+        ctx.spawn(
+            async move {
+                let raft_instance = Self::initialize_raft(settings).await;
+                addr.do_send(InitializationComplete(raft_instance));
+            }
+            .into_actor(self),
+        );
     }
 }
 
-impl Supervised for TopicRaftActor{}
-
+impl Supervised for TopicRaftActor {}
 
 impl Actor for TopicRaftActor {
     type Context = Context<Self>;
 }
 
-#[derive(Message)]
-#[rtype(result="()")]
+#[derive(Message, Clone)]
+#[rtype(result = "()")]
 struct InitializationComplete(Result<TopicRaft, TopicRaftError>);
 
 impl Handler<InitializationComplete> for TopicRaftActor {
@@ -125,7 +150,7 @@ impl Handler<InitializationComplete> for TopicRaftActor {
                 self.state = ActorState::Running;
                 log::info!("TopicRaftActor initialized successfully.");
                 self.process_pending_messages(ctx);
-            },
+            }
             Err(e) => {
                 log::error!("Failed to initialize TopicRaftActor: {}", e);
                 self.state = ActorState::Failed(e);
@@ -136,7 +161,7 @@ impl Handler<InitializationComplete> for TopicRaftActor {
 
 #[derive(Message, Clone, Debug)]
 #[rtype(result = "Result<(), TopicRaftError>")]
-pub struct Subscribe{
+pub struct Subscribe {
     node_id: NodeId,
     tenant_id: String,
     client_identifier: String,
@@ -145,49 +170,57 @@ pub struct Subscribe{
 }
 
 impl Handler<Subscribe> for TopicRaftActor {
-
     type Result = ResponseActFuture<Self, Result<(), TopicRaftError>>;
-
 
     fn handle(&mut self, msg: Subscribe, _: &mut Self::Context) -> Self::Result {
         match &self.state {
             ActorState::Initializing => {
                 log::warn!("TopicRaftActor is initializing, message will be queued.");
                 self.pending_messages.push(Box::new(msg));
-                return Box::pin(async { Err(TopicRaftError::NotReady("Initializing".to_string())) }.into_actor(self));
-            },
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Initializing".to_string())) }
+                        .into_actor(self),
+                );
+            }
             ActorState::Running => {
                 let raft = self.raft.clone();
-                return Box::pin(async move {
-                    if let Some(raft_instance) = raft.get() {
-                        let command = crate::raft::topic::types::Request::SubscribeTopic {
-                            node_id: msg.node_id,
-                            tenant_id: msg.tenant_id,
-                            client_identifier: msg.client_identifier,
-                            topic: msg.topic,
-                            qos: msg.qos,
-                        };
-                        raft_instance.client_write(command).await?;
-                        Ok(())
-                    } else {
-                        Err(TopicRaftError::NotInitialized)
+                return Box::pin(
+                    async move {
+                        if let Some(raft_instance) = raft.get() {
+                            let command = crate::raft::topic::types::Request::SubscribeTopic {
+                                node_id: msg.node_id,
+                                tenant_id: msg.tenant_id,
+                                client_identifier: msg.client_identifier,
+                                topic: msg.topic,
+                                qos: msg.qos,
+                            };
+                            raft_instance.client_write(command).await?;
+                            Ok(())
+                        } else {
+                            Err(TopicRaftError::NotInitialized)
+                        }
                     }
-                }.into_actor(self));
-            },
+                    .into_actor(self),
+                );
+            }
             ActorState::Failed(e) => {
                 let e = e.clone();
-                return Box::pin(async move { 
-                    Err(TopicRaftError::ServiceUnavailable(e.to_string())) 
-                }.into_actor(self));
-            },
+                return Box::pin(
+                    async move { Err(TopicRaftError::ServiceUnavailable(e.to_string())) }
+                        .into_actor(self),
+                );
+            }
             ActorState::Stopped => {
-                return Box::pin(async { Err(TopicRaftError::NotReady("Actor is stopped".to_string())) }.into_actor(self));
-            },
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Actor is stopped".to_string())) }
+                        .into_actor(self),
+                );
+            }
         };
     }
 }
 
-#[derive(Message)]
+#[derive(Message, Clone, Debug)]
 #[rtype(result = "Result<(), TopicRaftError>")]
 pub struct Unsubscribe {
     node_id: NodeId,
@@ -197,27 +230,56 @@ pub struct Unsubscribe {
 }
 
 impl Handler<Unsubscribe> for TopicRaftActor {
-
     type Result = ResponseActFuture<Self, Result<(), TopicRaftError>>;
 
     fn handle(&mut self, msg: Unsubscribe, _: &mut Self::Context) -> Self::Result {
-        let raft = self.raft.clone();
-        Box::pin(async move {
-            let command = crate::raft::topic::types::Request::UnsubscribeTopic {
-                node_id: msg.node_id,
-                tenant_id: msg.tenant_id,
-                client_identifier: msg.client_identifier,
-                topic: msg.topic,
-            };
-            raft.get().unwrap().client_write(
-                command
-            ).await?;
-            Ok(())
-        }.into_actor(self))
+        match &self.state {
+            ActorState::Initializing => {
+                log::warn!("TopicRaftActor is initializing, message will be queued.");
+                self.pending_messages.push(Box::new(msg));
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Initializing".to_string())) }
+                        .into_actor(self),
+                );
+            }
+            ActorState::Running => {
+                let raft = self.raft.clone();
+                return Box::pin(
+                    async move {
+                        if let Some(raft_instance) = raft.get() {
+                            let command = crate::raft::topic::types::Request::UnsubscribeTopic {
+                                node_id: msg.node_id,
+                                tenant_id: msg.tenant_id,
+                                client_identifier: msg.client_identifier,
+                                topic: msg.topic,
+                            };
+                            raft_instance.client_write(command).await?;
+                            Ok(())
+                        } else {
+                            Err(TopicRaftError::NotInitialized)
+                        }
+                    }
+                    .into_actor(self),
+                );
+            }
+            ActorState::Failed(e) => {
+                let e = e.clone();
+                return Box::pin(
+                    async move { Err(TopicRaftError::ServiceUnavailable(e.to_string())) }
+                        .into_actor(self),
+                );
+            }
+            ActorState::Stopped => {
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Actor is stopped".to_string())) }
+                        .into_actor(self),
+                );
+            }
+        };
     }
 }
 
-#[derive(Message)]
+#[derive(Message, Clone, Debug)]
 #[rtype(result = "Result<(), TopicRaftError>")]
 pub struct RegisterRetainPublishPacket {
     tenant_id: String,
@@ -226,26 +288,52 @@ pub struct RegisterRetainPublishPacket {
 }
 
 impl Handler<RegisterRetainPublishPacket> for TopicRaftActor {
-
     type Result = ResponseActFuture<Self, Result<(), TopicRaftError>>;
 
     fn handle(&mut self, msg: RegisterRetainPublishPacket, _: &mut Self::Context) -> Self::Result {
-        let raft = self.raft.clone();
-        Box::pin(async move {
-            let command = crate::raft::topic::types::Request::RegisterRetainPublishPacket {
-                tenant_id: msg.tenant_id,
-                source_client_identifier: msg.client_id,
-                publish_packet: msg.publish_packet,
-            };
-            raft.get().unwrap().client_write(
-                command
-            ).await?;
-            Ok(())
-        }.into_actor(self))
+        match &self.state {
+            ActorState::Initializing => {
+                log::warn!("TopicRaftActor is initializing, message will be queued.");
+                self.pending_messages.push(Box::new(msg));
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Initializing".to_string())) }
+                        .into_actor(self),
+                );
+            }
+            ActorState::Running => {
+                let raft = self.raft.clone();
+                return Box::pin(
+                    async move {
+                        let command =
+                            crate::raft::topic::types::Request::RegisterRetainPublishPacket {
+                                tenant_id: msg.tenant_id,
+                                source_client_identifier: msg.client_id,
+                                publish_packet: msg.publish_packet,
+                            };
+                        raft.get().unwrap().client_write(command).await?;
+                        Ok(())
+                    }
+                    .into_actor(self),
+                );
+            }
+            ActorState::Failed(e) => {
+                let e = e.clone();
+                return Box::pin(
+                    async move { Err(TopicRaftError::ServiceUnavailable(e.to_string())) }
+                        .into_actor(self),
+                );
+            }
+            ActorState::Stopped => {
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Actor is stopped".to_string())) }
+                        .into_actor(self),
+                );
+            }
+        }
     }
 }
 
-#[derive(Message)]
+#[derive(Message, Clone, Debug)]
 #[rtype(result = "Result<(), TopicRaftError>")]
 pub struct CleanRetainPublishPacket {
     tenant_id: String,
@@ -253,20 +341,46 @@ pub struct CleanRetainPublishPacket {
 }
 
 impl Handler<CleanRetainPublishPacket> for TopicRaftActor {
-
     type Result = ResponseActFuture<Self, Result<(), TopicRaftError>>;
 
     fn handle(&mut self, msg: CleanRetainPublishPacket, _: &mut Self::Context) -> Self::Result {
-        let raft = self.raft.clone();
-        Box::pin(async move {
-            let command = crate::raft::topic::types::Request::CleanRetainPublishPacket {
-                tenant_id: msg.tenant_id,
-                topic_filter: msg.topic_filter,
-            };
-            raft.get().unwrap().client_write(
-                command
-            ).await?;
-            Ok(())
-        }.into_actor(self))
+        match &self.state {
+            ActorState::Initializing => {
+                log::warn!("TopicRaftActor is initializing, message will be queued.");
+                self.pending_messages.push(Box::new(msg));
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Initializing".to_string())) }
+                        .into_actor(self),
+                );
+            }
+            ActorState::Running => {
+                let raft = self.raft.clone();
+                return Box::pin(
+                    async move {
+                        let command =
+                            crate::raft::topic::types::Request::CleanRetainPublishPacket {
+                                tenant_id: msg.tenant_id,
+                                topic_filter: msg.topic_filter,
+                            };
+                        raft.get().unwrap().client_write(command).await?;
+                        Ok(())
+                    }
+                    .into_actor(self),
+                );
+            }
+            ActorState::Failed(e) => {
+                let e = e.clone();
+                return Box::pin(
+                    async move { Err(TopicRaftError::ServiceUnavailable(e.to_string())) }
+                        .into_actor(self),
+                );
+            }
+            ActorState::Stopped => {
+                return Box::pin(
+                    async { Err(TopicRaftError::NotReady("Actor is stopped".to_string())) }
+                        .into_actor(self),
+                );
+            }
+        }
     }
 }
