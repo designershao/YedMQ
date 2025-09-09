b5ce0ac588a8442b5565869acb831b32c5679a19
designershao <designer.shaoyan@gmail.com>
2025-09-09 09:55:37 +0800

    🚧 Add plugin host module with build configuration and initial PluginManager struct; update workspace dependencies and proto definitions

---
+++ plugin_host/build.rs
@@ -0,0 +1,28 @@
+// build.rs
+use prost_build::Config;
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let workspace_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("Workspace directory: {}", workspace_dir);
+    let protocol_crate_dir = format!("{}/../plugin_protocol", workspace_dir);
+    let proto_root = format!("{}/../plugin_protocol/proto", workspace_dir);
+    let proto_files = &[format!("{}/proto/yedmq_plugin_protocol.proto", protocol_crate_dir)];
+    
+    // 配置 prost 编译器
+    let mut config = Config::new();
+    config
+        .out_dir(PathBuf::from(env::var("OUT_DIR").unwrap()))
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .type_attribute(".", "#[serde(rename_all = \"camelCase\")]")
+        .type_attribute("ErrorInfo", "#[derive(Clone, PartialEq)]")
+        .type_attribute("ProtocolMessage", "#[derive(Clone)]")
+        .compile_protos(proto_files, &[proto_root])?;
+
+    for proto_file in proto_files {
+        println!("cargo:rerun-if-changed={}", proto_file);
+    }
+
+    Ok(())
+}
