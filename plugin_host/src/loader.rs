5752f1bdafe980079c98343e6c29142b14662a62
designershao <designer.shaoyan@gmail.com>
2026-04-23 16:14:35 +0800

    🐛 Handle relative paths correctly in plugin loader.

--- plugin_host/src/loader.rs
+++ plugin_host/src/loader.rs
@@ -217,6 +217,7 @@ impl PluginLoader {
                     #[cfg(not(windows))]
                     let exe_path = plugin_dir.join(executable);
 
+
                     #[cfg(windows)]
                     if exe_path.extension().is_none() && !exe_path.exists() {
                         let candidate = exe_path.with_extension("exe");
@@ -233,7 +234,9 @@ impl PluginLoader {
                         );
                     }
 
-                    let mut cmd = Command::new(&exe_path);
+                    let abs_path = std::fs::canonicalize(exe_path.clone())?;
+
+                    let mut cmd = Command::new(&abs_path);
 
                     if let Some(working_dir) = &manifest.runtime.working_dir {
                         let working_dir_path = plugin_dir.join(working_dir);
@@ -244,9 +247,11 @@ impl PluginLoader {
                                 working_dir_path.display()
                             );
                         }
-                        cmd.current_dir(working_dir_path);
+                        let working_dir_abs_path = std::fs::canonicalize(working_dir_path)?;
+                        cmd.current_dir(working_dir_abs_path);
                     } else {
-                        cmd.current_dir(&plugin_dir);
+                        let plugin_dir_abs_path = std::fs::canonicalize(plugin_dir)?;
+                        cmd.current_dir(&plugin_dir_abs_path);
                     }
 
                     cmd.env("RUST_LOG", "info");
